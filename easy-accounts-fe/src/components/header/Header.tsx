89b70b960e744d5c0cd4240c412477c17b1a700c
nuviosoftware <dev@nuviosoftware.co.uk>
2017-10-12 16:50:02 +0100

    #7 add boostrap add navbar

--- easy-accounts-fe/src/components/header/Header.tsx
+++ easy-accounts-fe/src/components/header/Header.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import './Header.css';
+import Navigation from '../navigation/Navigation';
 
 class Header extends React.Component {
   render() {
@@ -7,6 +8,8 @@ class Header extends React.Component {
         <div className="Header-component">
           <h1>Easy Accounts</h1>
           <p>A simple account keeping software by <a href="http://nuviosoftware.co.uk" target="_blank">Nuvio Software solutions</a>.</p>
+        
+          <Navigation />
         </div>
     );
   }
