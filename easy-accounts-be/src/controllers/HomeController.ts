ca01e35d5d242a89c7d3a8b7b167536aa4866284
nuviosoftware <dev@nuviosoftware.co.uk>
2017-11-15 13:14:35 +0000

    #1 delete node backend

--- easy-accounts-be/src/controllers/HomeController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-/**
- * GET /
- * Home page.
- */
-export let index = (req: Request, res: Response) => {
-  res.json("{message: 'Server running'}");
-};
